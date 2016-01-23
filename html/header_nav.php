6d0229b11975baa3dac62eb610d68642a4c6d71c
John Calhoun <johnmcalhou123@gmail.com>
2016-01-23 12:00:01 -0600

    page title link now goes to home page

--- html/header_nav.php
+++ html/header_nav.php
@@ -1,5 +1,5 @@
-<h2 class="hidden-xs hidden-sm"><a href="/"><?php bloginfo('name'); ?></a><span style="color: #10B897">: </span><b><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></b></h2>
-<h2 class="hidden-md hidden-lg text-center"><a href="/"><? bloginfo('name'); ?></a><span style="color: #10B897">: </span><b><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></b></h2>
+<h2 class="hidden-xs hidden-sm"><a href="<?php echo get_home_url(); ?>" ><?php bloginfo('name'); ?></a><span style="color: #10B897">: </span><b><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></b></h2>
+<h2 class="hidden-md hidden-lg text-center"><a href="<?php echo get_home_url();?>"><? bloginfo('name'); ?></a><span style="color: #10B897">: </span><b><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></b></h2>
 <?php wp_nav_menu( array('main_menu'=>'Main menu')); ?>
 <hr />
 
