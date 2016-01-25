4e550827a1a62f010428bf2bc770a0b31a3a9bd4
John Calhoun <johnmcalhou123@gmail.com>
2016-01-25 16:51:55 -0600

    fix #21 cleaned up header defs

--- php/single.php
+++ php/single.php
@@ -9,14 +9,17 @@
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-				<?php the_content('<p class="serif">Read the rest of this entry &raquo;</p>'); ?>
+				<?php the_content('<p>Read the rest of this entry &raquo;</p>'); ?>
 
 				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
 
 				<p>
 					<small>
-						
-						
+                         <button class="button" onclick="$('#Comments').toggle();">Comments</button>
+                         <div id="Comments" style="display: none">
+                             <?php comments_template(); ?>		
+                         </div>
+                        						
 					</small>
 				</p>
 
