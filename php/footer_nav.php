7a11db03de14afc48544dbf1330780246f7a5ab8
John Calhoun <johnmcalhou123@gmail.com>
2016-01-26 23:45:21 -0600

    fix #26

--- php/footer_nav.php
+++ php/footer_nav.php
@@ -1,4 +1,10 @@
 <div class="col-md-6 col-md-offset-2">
-<?php wp_nav_menu( array('main_menu'=>'Main menu')); ?>
+<?php wp_nav_menu( array(
+                    'main_menu'=>'Main menu',
+                    'depth'=>1,
+                    'menu_class'=>'main_menu',
+                    'before'=>'',
+                    'after'=> ''
+               )); ?> 
 </div>
 </div>
--- php/header_nav.php
+++ php/header_nav.php
@@ -1,6 +1,12 @@
 <h2 class="hidden-xs hidden-sm"><a href="<?php echo get_home_url(); ?>" ><?php bloginfo('name'); ?></a><span style="color: #10B897">: </span><b><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></b></h2>
 <h2 class="hidden-md hidden-lg text-center"><a href="<?php echo get_home_url();?>"><? bloginfo('name'); ?></a><span style="color: #10B897">: </span><b><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></b></h2>
-<?php wp_nav_menu( array('main_menu'=>'Main menu')); ?>
+<?php wp_nav_menu( array(     
+          'main_menu'=>'Main menu',
+          'depth'=>1,
+          'menu_class'=>'main_menu',
+          'before'=>'',
+          'after'=> ''
+          )); ?>
 <hr />
 
 
