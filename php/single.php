0577020ecdd242af948d71c94bf8795746aac662
John Calhoun <johnmcalhou123@gmail.com>
2016-01-26 22:04:34 -0600

    fix #36 copied comments.php from twentyfifteen

--- php/single.php
+++ php/single.php
@@ -6,32 +6,21 @@
 
 			<?php get_template_part('header_nav'); ?>
 			<div class="entry">
-
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-				<?php the_content('<p>Read the rest of this entry &raquo;</p>'); ?>
-
-				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
-
-				<p>
-					<small>
-                         <button class="button" onclick="$('#Comments').toggle();">Comments</button>
-                         <div id="Comments" style="display: none">
-                              <?php comment_form(); ?>
-                              <?php comments_template(); ?>		
-                         </div>
-                        						
-					</small>
-				</p>
-
-			</div>
+     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+          <?php if( isset($_GET['comments']) && ($_GET['comments']==1)): ?>
+               <a href="<?php the_permalink(); ?>" rel="bookmark">Return to Post</a>
+               <?php comments_template(); ?>
+          <?php else :?>
+			<?php the_content('<p>Read the rest of this entry &raquo;</p>'); ?>
+			<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+               <a href="<?php the_permalink(); ?>&comments=1">Comments</a>
+               (<?php comments_number(); ?>)
+		<?php endif; ?>	
+               </div>
 		</div>
-
 	<?php endwhile; else: ?>
-
 		<p>Sorry, no posts matched your criteria.</p>
-
-<?php endif; ?>
+     <?php endif; ?>
 	<small><?php edit_post_link('Leo edit link', '<p>', '</p>'); ?></small>
 
 	</div>
