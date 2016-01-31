32ffd84c6ecaafddcc54f39800216422247b7c07
John Calhoun <johnmcalhou123@gmail.com>
2016-01-30 18:44:04 -0600

    tag and category posts are left and right aligned.

--- php/index.php
+++ php/index.php
@@ -13,8 +13,14 @@
                     <?php the_excerpt(); ?>
                </div>
                <div class="entry-meta">
-                    <p><?php the_tags('',':',''); ?></p>
-                    <p><?php the_category(':'); ?></p>
+                    <div class="row">
+                    <div class="col-xs-6 text-left">
+                         <p><?php the_tags('',':',''); ?></p>
+                    </div>
+                    <div class="col-xs-6 text-right">
+                         <p><?php the_category(':'); ?></p>
+                    </div>
+                    </div>
           	</div>
                </div>
 		<?php endwhile; ?>
