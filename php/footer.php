ed6caa085ebf5598db592046b3f0de2494dea7c9
John Calhoun <johnmcalhou123@gmail.com>
2016-01-30 21:54:05 -0600

    fix #50 moved javascript load to footer

--- php/footer.php
+++ php/footer.php
@@ -7,5 +7,8 @@
 </div><!--id close -->
 <?php wp_footer(); ?>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery. min.js"><script>
+<script src="javascripts/bootstrap.js"></script>  
+
 </body>
 </html>
--- php/header.php
+++ php/header.php
@@ -3,15 +3,11 @@
 
 <head profile="http://gmpg.org/xfn/11">
 	<meta http-equiv="Content-Type" name="viewport" content="width=device-width, initial-scale=1, <?php bloginfo('html_type'); ?>, charset=<?php bloginfo('charset'); ?>" />
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	<script src="javascripts/bootstrap.js"></script>
 	<link href='https://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
 	<title><?php wp_title(':'); ?></title>
-
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen, print" />
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
