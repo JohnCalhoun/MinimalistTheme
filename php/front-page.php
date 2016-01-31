75dd8bd75149ffd269f94d9e276ecf78ca1ef8a0
John Calhoun <johnmcalhou123@gmail.com>
2016-01-30 21:39:58 -0600

    added blog tag line to frontpage

--- php/front-page.php
+++ php/front-page.php
@@ -5,8 +5,9 @@
 		<div class="post" id="post-<?php the_ID(); ?>">
 			<div class="entry">
 
+          <p><?php bloginfo( 'description' ); ?></p>
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
+          
 				<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
 
 				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
