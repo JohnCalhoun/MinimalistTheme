e0446d58a0ce3d00be6ec2be52685c420d3053b8
John Calhoun <johnmcalhou123@gmail.com>
2016-01-31 09:34:48 -0600

    fix #76

---
+++ php/archives.php
@@ -0,0 +1,42 @@
+<?php 
+/*
+Template Name: Archives
+*/
+get_header(); ?>
+
+	<div id="content">
+	<div class="row">
+	<div class="col-md-8">
+<?php if (have_posts()) : ?>
+          <?php the_post(); ?>
+          <h1 class="entry-title"><?php the_title(); ?></h1>
+          
+          <?php get_search_form(); ?>
+          <div class=row>
+          <div class="col-md-6">
+               <h2>Archives by Month:</h2>
+               <ul>
+                    <?php wp_get_archives('type=monthly'); ?>
+               </ul>
+          </div>
+          <div class="col-md-6">
+               <h2>Archives by Subject:</h2>
+               <ul>
+                    <?php wp_list_categories(); ?>
+               </ul>
+          </div>
+          </div>
+<?php else : ?>
+
+		<h2 class="center">Not Found</h2>
+		<p class="center">Sorry, but you are looking for something that isn't here.</p>
+<?php endif; ?>
+
+	</div>
+<div class="col-md-4 hidden-xs hidden-sm">
+     <?php get_sidebar('archive'); ?>
+</div>
+</div>
+</div>
+
+<?php get_footer(); ?>
--- php/functions.php
+++ php/functions.php
@@ -73,6 +73,17 @@
                     'after_title' => '</h3>'
                )
           );
+          register_sidebar(
+               array(
+                    'id' => 'archive-sidebar',
+                    'name' => ( 'Archive Sidebar' ),
+                    'description' => ( 'Sidebar for the Archive pages' ),
+                    'before_widget' => '<div id="%1$s" class="widget %2$s">',
+                    'after_widget' => '</div>',
+                    'before_title' => '<h3 class="widget-title">',
+                    'after_title' => '</h3>'
+               )
+          );
 
      /* Repeat register_sidebar() code for additional sidebars. */
      }
