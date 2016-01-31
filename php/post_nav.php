e602fae5f9fa6dd90b61eaf66897761ca4b72c20
John Calhoun <johnmcalhou123@gmail.com>
2016-01-30 21:07:11 -0600

    fix #42 rewrote the nav shortened nav to 10 entries

--- php/post_nav.php
+++ php/post_nav.php
@@ -1,5 +1,5 @@
-<table>
-<tr><th><br /><br /><h4><b>POSTS</b></h4></th></tr>
+<div id="post_nav">
+<h4><b>POSTS</b></h4>
 <?php
 $query = "SELECT YEAR(post_date) AS `year`, MONTH(post_date) as `month`, DAYOFMONTH(post_date) as `dayofmonth`, ID, post_name, post_title FROM $wpdb->posts WHERE post_type = 'post' AND post_status = 'publish' ORDER BY post_date DESC";
 $key = md5($query);
@@ -14,26 +14,29 @@ if ( !isset( $cache[ $key ] ) ) {
 if ($arcresults) {
   $last_year = 0;
   $last_month = 0;
+  $counter=0;
   foreach ( $arcresults as $arcresult ) {
+    if(++$counter == 10 ) break;
     $year = $arcresult->year;
     $month = $arcresult->month;
     if ($year != $last_year) {
       $last_year = $year;
       $last_month = 0;
 ?>
-<tr class=year><th><br /><br /><?php echo $arcresult->year; ?></th></tr>
+<div class="year"><?php echo $arcresult->year; ?></div>
 <?php
     }
     if ($month != $last_month) {
       $last_month = $month;
 ?>
-<tr class=archive><th><?php echo $GLOBALS['wp_locale']->get_month($arcresult->month) ?></th><td></td></tr>
+<div class="month"><?php echo $GLOBALS['wp_locale']->get_month($arcresult->month) ?></div>
 <?php
     }
 ?>
-<tr class=archive><th><?php echo $arcresult->dayofmonth; ?></th><td id=p<?php echo $arcresult->ID; ?>><a href="<?php echo get_permalink($arcresult->ID); ?>"><?php echo strip_tags(apply_filters('the_title', $arcresult->post_title)); ?></a></td></tr>
+<div class="day"><?php echo $arcresult->dayofmonth; ?></div>
+<div class="title"><span id=p<?php echo $arcresult->ID; ?>><a href="<?php echo get_permalink($arcresult->ID); ?>"><?php echo strip_tags(apply_filters('the_title', $arcresult->post_title)); ?></span></div>
 <?php
   }
 }
 ?>
-</table>
+</div>
